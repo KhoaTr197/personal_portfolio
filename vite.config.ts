98189750b3f4b6c6447c6456eb9e1d46a4a8b9b1
KhoaTr197 <darkreaper197@gmail.com>
2025-06-22 22:07:45 +0700

    [Refactor]: Install @types/node and setup alias config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from "path"
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
@@ -6,8 +7,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
-      'public': '/public',
+      '@': path.resolve(__dirname, "./src"),
+      'public': path.resolve(__dirname, "./public"),
     },
   },
 })
