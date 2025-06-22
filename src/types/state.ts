90637f519a0ebfdf70f3189aba344b0843272aae
KhoaTr197 <darkreaper197@gmail.com>
2025-06-22 22:09:20 +0700

    [Refactor]: Change @types to types for clarity and alias-access

---
+++ src/types/state.ts
@@ -0,0 +1,39 @@
+import { ReactNode } from "react";
+
+// App.tsx
+export interface CornerInfoContent {
+  left: string[],
+  right: string[],
+  className?: string;
+}
+
+// ShowcasePage.tsx
+export interface Project {
+  name: string;
+  description: string;
+  image: string;
+  url: string;
+}
+
+// AboutPage.tsx
+export interface AboutContent {
+  summary: string;
+  totalProjects: number;
+  yearsExperience: number;
+}
+
+// 3DSkillsetPage.tsx
+export interface SelectedSkill {
+  name: string;
+  description: string;
+}
+
+export interface Skill {
+  name: string;
+  url: string;
+  icon: {
+    component: ReactNode;
+    forceFill?: string;
+  };
+  description: string;
+}
