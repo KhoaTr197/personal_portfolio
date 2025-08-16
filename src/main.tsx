33dbb2ab8149d0729320e4fb3a3e07c90a9a8539
KhoaTr197 <darkreaper197@gmail.com>
2025-08-16 19:38:42 +0700

    [Fix] Correct the right Vercel analytics for Vite

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import '@/font.css'
 import '@/index.css'
 import App from '@/App.tsx'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { DeviceTypeProvider } from '@/context/DeviceTypeContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
