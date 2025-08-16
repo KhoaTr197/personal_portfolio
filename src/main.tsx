cd669b75ad4ada33f077efe2f8ad1b8a0f363f98
KhoaTr197 <darkreaper197@gmail.com>
2025-08-16 19:32:34 +0700

    [Feat] Add Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import '@/font.css'
 import '@/index.css'
 import App from '@/App.tsx'
+import { Analytics } from "@vercel/analytics/next"
 import { DeviceTypeProvider } from '@/context/DeviceTypeContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <DeviceTypeProvider>
       <App />
+      <Analytics />
     </DeviceTypeProvider>
   </StrictMode>,
 )
